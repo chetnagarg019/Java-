5e9271c221d5c5d63d059b1e72d3f61311df4e42
chetna <chetnagarg019@gmail.com>
2026-03-17 13:43:13 +0530

    commit #2

---
+++ SumLowestElements.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SumLowestElements {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input size of array
+        System.out.print("Enter size of array (N): ");
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        // Input array elements
+        System.out.println("Enter " + N + " elements:");
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Input M
+        System.out.print("Enter number of lowest elements to sum (M): ");
+        int M = sc.nextInt();
+
+        // Sort array
+        Arrays.sort(arr);
+
+        int sum = 0;
+
+        // Sum of lowest M elements
+        for (int i = 0; i < M; i++) {
+            sum += arr[i];
+        }
+
+        // Output
+        System.out.println("Sum of lowest " + M + " elements is: " + sum);
+
+        sc.close();
+    }
+}
---
+++ array.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class array {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        int rows = 3;
+        int col = 3;
+        int key = 8;
+        boolean flag = false;
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < col; j++){
+                if(key == matrix[i][j]){
+                    flag = true;
+                    break;
+
+                }
+            }
+            System.out.println("");
+        }
+
+        if(flag == true){
+            System.out.println("Key is find : " + key);
+        }else{
+            System.out.println("Not find");
+        }
+
+        
+
+        sc.close();
+
+        
+
+
+
+
+
+
+
+
+    }
+
+    
+}
