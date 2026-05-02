27206a873838e4382ef2275baab678121a6b584c
chetna <chetnagarg019@gmail.com>
2026-05-02 23:56:03 +0530

    commit #23

--- SumLowestElements.java
+++ SumLowestElements.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
-public class SumLowestElements {
+public class SumLowestElements { /// java sheet program 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in); //scan the size 
