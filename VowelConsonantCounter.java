72c9fa69142eb66c336bde27e9726e6ca079f737
chetna <chetnagarg019@gmail.com>
2026-05-07 22:02:33 +0530

    commit #27

--- VowelConsonantCounter.java
+++ VowelConsonantCounter.java
@@ -21,7 +21,7 @@ public class VowelConsonantCounter {
                 if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
                     vowels++;
                 } else {
-                    consonants++;
+                    consonants++; // it is a vowel consonent parts 
                 }
             }
         }
